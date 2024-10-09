f584f7badd9e0ee10b98450cf91e044739eea288
THIAGO LUCIANO DA SILVEIRA <thiago_l_silveira@estudante.sesisenai.org.br>
2024-10-09 08:09:47 -0300

    feat: Adicionar database

--- config.php
+++ config.php
@@ -2,5 +2,9 @@
 declare(strict_types = 1);
 
 define("VIEW_PATH", "");
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASS", "root");
+define("DB_NAME", "EPTRAN_DS24M2");
 
 include "Router.php";
