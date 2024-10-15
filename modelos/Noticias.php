b08aba504dca0f440c7bc5c14fa7206f2b731c2b
NICOLAS MATEUS FRANCISCO <nicolas_francisco@estudante.sesisenai.org.br>
2024-10-15 11:52:38 -0300

    feat: criar metodo de editar noticia

--- modelos/Noticias.php
+++ modelos/Noticias.php
@@ -1,25 +1,39 @@
 <?php
 
 class Noticia extends Database{
-    private $table = 'noticia';
-    public function __construct(){
-        parent::__construct();
+  private $table = 'noticia';
+  public function __construct(){
+    parent::__construct();
+  }
+  
+  public function adicionar(array $valores) {
+    
+    try {
+      $this->criar($this->table, [
+        'titulo'=> $valores["titulo"],
+        'conteudo'=> $valores["conteudo"],
+        'url_imagem'=> $valores["url_imagem"],
+        'id_administrador'=> $valores["id_administrador"],
+      ]);
+    } catch (Exception $e) {
+      return false;
+    }            
+    return true;
+  }
+  
+  public function editar(string $table, array $valores, $id) {
+    
+    $kao = [];
+    
+    foreach ($valor as $key => $value) {
+      array_push($kao, "$key = $value"); 
     }
 
-    public function adicionar(array $valores){
-        
-        try {
-            $this->criar($this->table, [
-                'titulo'=> $valores["titulo"],
-                'conteudo'=> $valores["conteudo"],
-                'url_imagem'=> $valores["url_imagem"],
-                'id_administrador'=> $valores["id_administrador"],
-            ]);
-        } catch (Exception $e) {
-            return false;
-        }
-            
-        return true;
-    }
+    $editar_valores = implode(", ", $kao);
+
+    $raw = "UPDATE $this->table SET $editar_valores WHERE id = $id"
+
+    $this->
+  }
 }
 
