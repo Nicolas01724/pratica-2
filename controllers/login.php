6ef4d325f77201bb97ae0b726024aef2d85ab9db
JONAS EMANUEL DOS SANTOS HENCHEL <jonas_henchel@estudante.sesisenai.org.br>
2024-10-29 12:42:47 -0300

    Fix: arrumar commit

--- controllers/login.php
+++ controllers/login.php
@@ -1,8 +1,23 @@
 <?php
 require_once ROOT_PATH . MODEL_PATH . "/Login.php";
 
-$login = new Login();
+
+
 
 class Login_controller extends Controller{
-    public function POST(): void{}
+    public function POST(): void{
+
+        $login = new Login();
+        $existe = $login->verificar_email($_POST['email']);
+
+       if($existe == false){
+           header('HTTP/1.1 201 Created');
+           die("Usuario invalido");
+       }
+
+        if(!assert_array_keys(['email','senha'], $_POST)){
+            header('HTTP/1.1 400 Bad Request');
+            die("Falta dados!");
+        }
+    }
 }
