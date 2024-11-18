0068aa09586ff50227a2274321bcc6c3f9544051
NICOLAS MATEUS FRANCISCO <nicolas_francisco@estudante.sesisenai.org.br>
2024-11-18 09:51:51 -0300

    Arumar errinho no banco

--- db.sql
+++ db.sql
@@ -1,3 +1,5 @@
+
+
 CREATE DATABASE pratica_nicolas;
 
 USE pratica_nicolas;
@@ -6,7 +8,7 @@ CREATE TABLE usuario (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     email VARCHAR(120) NOT NULL,
-    senha VARCHAR(30) NOT NULL
+    telefone VARCHAR(13) NOT NULL
 );
 
 
@@ -14,17 +16,20 @@ CREATE TABLE responsavel (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(45) NOT NULL,
     email VARCHAR(120) NOT NULL,
-    senha VARCHAR(30) NOT NULL
+	telefone VARCHAR(13) NOT NULL
 );
 
 CREATE TABLE chamado (
 	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     descricao TEXT,
-    criticidade VARCHAR(5),
+    criticidade varchar(5),
     andamento INT,
     abertura DATE,
-    id_usuario INT NOT NULL,
     id_responsavel INT NOT NULL,
-    FOREIGN KEY (id_usuario) REFERENCES usuario(id),
-    FOREIGN KEY (id_responsavel) REFERENCES responsavel(id)
+    foreign key (id_responsavel) REFERENCES responsavel(id)
 );
+
+select * FROM USUARIO;
+select * from responsavel;
+SELECT * FROM responsavel;
+
